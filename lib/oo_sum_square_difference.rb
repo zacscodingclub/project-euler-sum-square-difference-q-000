0c95c2428418340eaff32117a9749bbe24b43857
zacscodingclub <zbaston@gmail.com>
2016-10-23 17:30:04 -0400

    Done.

--- lib/oo_sum_square_difference.rb
+++ lib/oo_sum_square_difference.rb
@@ -1,1 +1,20 @@
 # Implement your object-oriented solution here!
+
+class SumSquareDifference
+  def initialize(limit)
+  	@limit = limit
+    @arr = (1..limit).to_a
+  end
+
+  def sum_of_squares
+    @arr.inject(0) { |sum, x| sum += x **2 }
+  end
+
+  def square_of_sums
+    @arr.inject(0) { |sum, x| sum += x } ** 2
+  end
+
+  def difference
+    square_of_sums - sum_of_squares
+  end
+end
--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,1 +1,10 @@
 # Implement your procedural solution here!
+
+
+def sum_square_difference(limit)
+  arr = (1..limit).to_a
+  sum_squares = arr.inject(0) { |sum, x| sum += x **2 }
+  squared_sum = arr.inject(0) { |sum, x| sum += x } ** 2
+
+  squared_sum - sum_squares
+end
